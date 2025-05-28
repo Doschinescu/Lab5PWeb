c74dc52b96c200fe6e9d9f75ab0addacadc01b1a
Dan <114677896+Doschinescu@users.noreply.github.com>
2025-05-28 18:45:07 +0300

    Parse HTTP response and strip HTML tags for clean display

--- go2web.py
+++ go2web.py
@@ -1,19 +1,36 @@
-from parser import parse_args
+import socket
+import re
 
-def main():
-    args = parse_args()
+def strip_html_tags(html):
+    clean_text = re.sub('<[^<]+?>', '', html)  # Removes all tags like <tag>
+    return clean_text.strip()
 
-    if args.url:
-        print(f"Fetching URL: {args.url}")
-        # HTTP request code goes here
+def handle_url(url):
+    if not url.startswith("http://"):
+        url = "http://" + url
 
-    elif args.search:
-        search_term = ' '.join(args.search)
-        print(f"Searching for: {search_term}")
-        # Search engine logic goes here
+    host = url.split("//")[1].split("/")[0]
+    path = "/" + "/".join(url.split("//")[1].split("/")[1:])
 
-    else:
-        print("Use -h to see help.")
+    if path == "/":
+        path = "/"
 
-if __name__ == "__main__":
-    main()
+    port = 80
+    request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        s.sendall(request.encode())
+
+        response = b""
+        while True:
+            data = s.recv(1024)
+            if not data:
+                break
+            response += data
+
+    response_text = response.decode(errors='ignore')
+    headers, _, body = response_text.partition("\r\n\r\n")
+    
+    print("Clean Output:")
+    print(strip_html_tags(body))
